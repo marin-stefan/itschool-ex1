688255f28dcc4c8d3ece482a24a30a6b4fcce2fb
stefann06 <stefann06@yahoo.com>
2025-11-03 14:24:19 +0200

    removed class add

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const hamburgerIcon = document.querySelector(".menu-icon");
-
 const ulMenu = document.querySelector(".menu-icon-container ul");
-ulMenu.classList.add("show-dropdown");
 
 hamburgerIcon.addEventListener("click", function () {
     ulMenu.classList.toggle("show-dropdown");
 });
-
-
